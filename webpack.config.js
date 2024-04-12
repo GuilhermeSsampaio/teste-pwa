0c07d17c1fada20782e496d1ba235f931238e386
guilhermessampaio20 <guilhermessampaio20@gmail.com>
2024-04-12 12:35:00 -0400

    movendo pra raiz webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   // outras configurações do webpack
   plugins: [
     new InjectManifest({
-      swSrc: './src/service-worker.js',
+      swSrc: './public/service-worker.js',
       swDest: 'service-worker.js',
       maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB
     }),
