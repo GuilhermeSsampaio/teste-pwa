00990623e4853dcbd7581356f46d4caa0bf1a36b
guilhermessampaio20 <guilhermessampaio20@gmail.com>
2024-04-13 16:58:09 -0400

    estilo btn ios

--- components/InstallButtonIos.jsx
+++ components/InstallButtonIos.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-scroll';
 
 const InstallButtonIos = () => {
     const [showInstructions, setShowInstructions] = useState(false);
@@ -7,14 +8,29 @@ const InstallButtonIos = () => {
         setShowInstructions(true);
     };
 
+    const closeInstructions = () => {
+        setShowInstructions(false);
+    };
+
     return (
         <div>
-            <button onClick={showInstallInstructions}>Adicionar à Tela de Início</button>
+            <button className='btn btnIos' onClick={showInstallInstructions}>Adicionar à Tela de Início</button>
             {showInstructions && (
-                <div id="instructions">
-                    <p>Para instalar este app no seu iPhone:</p>
-                    <ol>
-                        <li>Abrir o Safari e navegar para [URL do seu PWA].</li>
+                <div id="instructions" style={{ 
+                    position: 'fixed', 
+                    top: '50%', 
+                    left: '50%', 
+                    transform: 'translate(-50%, -50%)', 
+                    backgroundColor: 'white', 
+                    padding: '20px', 
+                    borderRadius: '5px', 
+                    boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)', 
+                    zIndex: '9999' 
+                }}>
+                    <button className='btnIos button' onClick={closeInstructions} style={{ position: 'absolute', top: '10px', right: '10px' }}>Fechar</button>
+                    <p style={{ marginBottom: '10px' }}>Para instalar este app no seu iPhone:</p>
+                    <ol style={{ paddingLeft: '20px' }}>
+                        <li>Abrir o Safari e navegar para <Link href='https://teste-pwa-rho.vercel.app/'> [https://teste-pwa-rho.vercel.app/]</Link>.</li>
                         <li>Tocar no botão de compartilhamento na barra de navegação.</li>
                         <li>Deslizar e tocar em "Adicionar à Tela de Início".</li>
                         <li>Confirmar tocando em "Adicionar".</li>
