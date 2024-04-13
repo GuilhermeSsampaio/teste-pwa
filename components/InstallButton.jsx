3c00aae97f5224167cd1631c251735cd172b55e7
guilhermessampaio20 <guilhermessampaio20@gmail.com>
2024-04-13 15:53:52 -0400

    subindo função pra verificar se é ios

--- components/InstallButton.jsx
+++ components/InstallButton.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import Link from 'next/link';
 function InstallButton() {
   const [showInstallButton, setShowInstallButton] = useState(true); // Definido como true inicialmente
   const [deferredPrompt, setDeferredPrompt] = useState(null);
@@ -35,11 +35,25 @@ function InstallButton() {
   return (
     <>
       {showInstallButton && (
+    <div>
         <div>
           <button id='btn-instalar' className='btn' onClick={handleInstallClick}>
             Instalar
           </button>
         </div>
+        <div className='isIos'>
+            <button onClick={() => setShowInstructions(true)}>Adicionar à Tela de Início</button>
+            <div id="instructions">
+              <p>Para instalar este app no seu iPhone:</p>
+              <ol>
+                <li>Abrir o Safari e navegar para <Link href='https://teste-pwa-rho.vercel.app/'>https://teste-pwa-rho.vercel.app/</Link>.</li>
+                <li>Tocar no botão de compartilhamento na barra de navegação.</li>
+                <li>Deslizar e tocar em "Adicionar à Tela de Início".</li>
+                <li>Confirmar tocando em "Adicionar".</li>
+              </ol>
+            </div>
+    </div>
+          </div>
       )}
     </>
   );
